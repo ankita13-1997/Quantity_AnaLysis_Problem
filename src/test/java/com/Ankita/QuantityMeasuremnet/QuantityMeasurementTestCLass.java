b42d107de62f826e914e1c765638beb31bbf76c3
ankita13-1997 <parhiankita@gmail.com>
2021-04-24 16:02:35 +0530

    Ankita[added]:the code of checking the o value of 0 feet and a null value

--- src/test/java/com/Ankita/QuantityMeasuremnet/QuantityMeasurementTestCLass.java
+++ src/test/java/com/Ankita/QuantityMeasuremnet/QuantityMeasurementTestCLass.java
@@ -13,4 +13,13 @@ public class QuantityMeasurementTestCLass {
         Assert.assertEquals(feet1,feet2);
 
     }
+    @Test
+    public void When0Feet_AND_nullShouldReturnEqual(){
+        Feet feet1= new Feet(0.0);
+        boolean checknullValue=feet1.equals(null);
+        Assert.assertFalse(checknullValue);
+
+
+
+    }
 }
