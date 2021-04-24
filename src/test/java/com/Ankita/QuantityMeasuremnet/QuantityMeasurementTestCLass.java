8441503cf691f0bd9868a71a24226bd7a49fb105
ankita13-1997 <parhiankita@gmail.com>
2021-04-24 16:14:35 +0530

    Ankita[added]:the code of checking the o value of 0 feet and reference value  and two diiferent value Test Cases

--- src/test/java/com/Ankita/QuantityMeasuremnet/QuantityMeasurementTestCLass.java
+++ src/test/java/com/Ankita/QuantityMeasuremnet/QuantityMeasurementTestCLass.java
@@ -22,4 +22,25 @@ public class QuantityMeasurementTestCLass {
 
 
     }
+
+    @Test
+    public void When0Feet_AND_VAlue_withReferencehouldReturnEqual(){
+        Feet feet1= new Feet(0.0);
+        boolean checknullValue=feet1.equals(1);
+        Assert.assertFalse(checknullValue);
+
+
+
+    }
+
+    @Test
+    public void When0Feet_AND_1FeetReturnEqual(){
+        Feet feet1= new Feet(0.0);
+        Feet feet2=new Feet(1.2);
+        Assert.assertNotEquals(feet1,feet2);
+
+
+
+
+    }
 }
